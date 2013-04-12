07b016e6df18d6160f4edf8eab3360c29f8e11f0
mwchen <mwchen@mit.edu>
2013-04-11 20:38:23 -0400

    made address book into a modal layover

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,8 @@ $('document').ready(function() {
     $(".job-group").slimScroll({
         height: '160px'
     }); 
+    
+    loadAddressBook(); 
 });
 
 
@@ -74,5 +76,44 @@ function addJob(currentJob) {
             // there is a problem with this job. 
             console.log('current status is invalid');
         }            
+}
+
+// Load Address book component. 
+function loadAddressBook() {
+    //data model
+    var contactList = new Array();
+    var alan = new Contact("Alan Michelson", "617-584-2094", "michelson@mit.edu");
+    var becky = new Contact("Becky Folds", "617-543-1352", "beks@mit.edu");
+    var jenks = new Contact("Jenks Jenkinson", "617-239-8971", "jenks@mit.edu");
+    var homeDepot = new Contact("Home Depot", "617-940-0184", "contact@homedepot.com");
+    contactList.push(alan);
+    contactList.push(becky);
+    contactList.push(jenks);
+    contactList.push(homeDepot);
+    //console.log(contactList.length);
+    for(var c = 0; c < contactList.length; c++){
+    	//console.log(c);
+    	//var contacthtml = '<div class="contact" id="c' + c + '">' + contactList[c].name + '</div>';
+    	var contacthtml = '<div class="contact" id="c' + c + '">' + '</div>';
+    	//console.log(contacthtml);
+    	var topLoc = c*80;
+    	//console.log("#c" + c);
+    	console.log(topLoc);
+    	$("#table").append(contacthtml);
+    	var name = '<div class="name" id="n' + c + '">' + contactList[c].name + '</div>';
+    	var phone = '<div class="phone" id="p' + c + '">' + contactList[c].phone + '</div>';
+    	var email = '<div class="email" id="e' + c + '">' + contactList[c].email + '</div>';
+    	var contactImg = '<div class="imgdiv" id="i' + c + '"><img class="contactImg" src="images/default.png" /></div>';
+    	var contactText = '<div class="contactText" id="t' + c + '">' + name + phone + email + '</div';
+    	var typeImg = '<div><img class="typeImg" src="images/wrench.gif"/></div>';
+    	//$("#c" + c).append(name);
+    	//$("#c" + c).append(phone);
+    	$("#c" + c).append(contactImg);
+    	$("#c" + c).append(contactText);
+    	$("#c" + c).append(typeImg);
+    	$("#c" + c).css('top', topLoc + "px");
+    	$("#c" + c).css('height', 77 + "px");
+    	//$("#table").append('<div id="i"></div>');
+    }
 }
 
