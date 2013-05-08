01973195a0737bd71811de90a89eff2fd988eabe
Jeffrey Warren <jtwarren@mit.edu>
2013-05-08 00:24:26 -0400

    labels

--- src/js/main.js
+++ src/js/main.js
@@ -40,9 +40,8 @@ $('document').ready(function() {
         var label = snapshot.val();
 
         labelTypes.push(label.name)
-        labelColorPairs[label.name] = label.color
 
-        addNewLabel(label.name);
+        addNewLabel(label.name, label.color);
     });
 
 
@@ -116,25 +115,6 @@ $('document').ready(function() {
         }
     });
 
-    // $('#create-job-form').on('submit', function(event) {
-
-    //     var jobsRef = new Firebase("https://mit-fixit.firebaseio.com/jobs");
-
-    //     var location = $("#inputLocation").val();
-    //     var title = $("#inputTitle").val();
-    //     var desc = $("#inputDescription").val();
-
-    //     var jobRef = jobsRef.push({"location" : location, "title" : title, "text" : desc, "time" : (new Date()).getTime(), "reporter" : "michael", "status" : "new"});
-
-    //     jobRef.setPriority(1/(new Date()).getTime());
-
-    //     $("#inputLocation").val("");
-    //     $("#inputTitle").val("");
-    //     $("#inputDescription").val("");
-
-    //     $('#createJobModal').modal('hide');
-    // });
-
 
     /***** 
      * Right Panel 
@@ -153,8 +133,14 @@ $('document').ready(function() {
         document.getElementById("create-label-text").value = "";
         $("#myLabelCreator").hide();
         $(".modal-backdrop").hide();
-        labelTypes.push(name);
-        addNewLabel(name);
+        // labelTypes.push(name);
+
+
+        // addNewLabel(name);
+
+        var labelsRef = new Firebase("https://mit-fixit.firebaseio.com/labels");
+        labelsRef.push({"name" : name, "color" : "red"});
+
     });
 
     $(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
@@ -191,9 +177,9 @@ $('document').ready(function() {
 /**
  * Adds a new label on the left panel. 
  */
-function addNewLabel(labelName){
+function addNewLabel(labelName, color){
     //newLabel = labelName;
-    labelColorPairs[labelName] = "purple";
+    labelColorPairs[labelName] = color;
     var leftPanelHTML = "";
     labelTypes.sort();
     for(var i = 0; i < labelTypes.length; i++){
